8f21a0cb3a3a90bdd17a44b2200d3c670164266b
Xtail <xtail1996@yandex.ru>
2018-03-13 16:55:48 +0300

    Небольшой рефакторинг кода

--- app/LightTaskManager/models/todolistadapter.cpp
+++ app/LightTaskManager/models/todolistadapter.cpp
@@ -25,27 +25,11 @@ void TodolistAdapter::openRepository(QString directory)
     qDebug() << "open";
     m_todolistProcess->setWorkingDirectory(m_directory);
     m_todolistProcess->start("/usr/local/opt/todolist/bin/todolist list");
-
     connect(m_todolistProcess, SIGNAL(readyReadStandardOutput()), this, SLOT(readData()));
-
-    //connect(proc,SIGNAL(readyReadStandardOutput()), SLOT(readStdout()));
-
-    /*QFile repository(m_filePath);
-    if(!repository.open(QIODevice::ReadWrite | QIODevice::Truncate | QIODevice::Text))
-    {
-        QMessageBox(QMessageBox::Warning, "Ошибка", "Не могу открыть файл!").exec();
-    }
-    else
-    {
-        file.write(fileContent.toUtf8());
-        file.close();
-    }*/
 }
 
 void TodolistAdapter::readData()
 {
     QByteArray data = m_todolistProcess->readAllStandardOutput();
     qDebug() << "read" << QString::fromUtf8(data);
-    //*sout = proc->readAllStandardOutput();
-    //textEdit->insertPlainText(QString(*sout));
 }
