f09e4d9b9dec3a13141443cbb64a19211cb3e036
Xtail <xtail1996@yandex.ru>
2018-04-11 23:24:38 +0300

    Пустой путь по умолчанию

--- app/LightTaskManager/models/settingsmanager.cpp
+++ app/LightTaskManager/models/settingsmanager.cpp
@@ -88,7 +88,7 @@ void SettingsManager::set(QString group, QString key, QVariant value)
 void SettingsManager::generateDefaultSettings()
 {
     settings->beginGroup("General");
-        settings->setValue("TodoListBinPath", "/usr/local/opt/todolist/bin/todolist");
+        settings->setValue("TodoListBinPath", "");
     settings->endGroup();
 
     saveSettings();
